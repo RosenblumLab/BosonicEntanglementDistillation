fe82ee5bf0de06418e379ff6dbb8da6dbc2c4a89
Shahar Silberstein <shaharsil4@gmail.com>
2023-04-26 23:31:23 +0300

    Add basic simulation

---
+++ Simulation.py
@@ -0,0 +1,50 @@
+from State import State
+from Operator import Operator
+
+
+class Simulation:
+    def __init__(
+            self,
+            number_of_fock_states: int = 80,
+            number_of_rotations: int = 4,
+            number_of_parties: int = 1,
+            initial_state_name: str = 'pegg-barnett',
+            number_of_parity_sectors: int = 4,
+            *args,
+            **kwargs):
+
+        self.number_of_fock_states = number_of_fock_states
+        self.number_of_rotations = number_of_rotations
+        self.number_of_parties = number_of_parties
+        self.initial_state_name = initial_state_name
+        self.number_of_parity_sectors = number_of_parity_sectors
+
+        self.__initialize_states(*args, **kwargs)
+
+    def __initialize_states(self, *args, **kwargs):
+        self.initial_state = self.__create_initial_state(*args, **kwargs)
+        self.noisy_state = self.initial_state
+        self.__add_noise()
+        self.state_after_protocol = self.__run_protocol()
+
+    def __create_initial_state(self, *args, **kwargs) -> State:
+        return State.create(
+            self.initial_state_name,
+            self.number_of_fock_states,
+            self.number_of_parties,
+            self.number_of_rotations,
+            False,
+            *args,
+            **kwargs)
+
+    def __add_noise(self):
+        pass
+
+    def __run_protocol(self) -> State:
+        wigner_parity_operator = Operator.create(
+            'wigner-parity',
+            number_of_fock_states=self.number_of_fock_states,
+            number_of_rotated_parts=self.number_of_parity_sectors,
+            number_of_parties=self.number_of_parties)
+
+        return self.noisy_state.evaluate_operator(wigner_parity_operator)
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import numpy as np
 import qutip
+import matplotlib.pyplot as plt
 
-from State import State
-from Operator import Operator
+from Simulation import Simulation
 
 if __name__ == '__main__':
-   pegg_barnett = State.create("pegg-barnett", 30)
-   Operator = Operator.create("rotation", 30, np.pi)
-   print(qutip.fidelity(State.create("pegg-barnett", 30, angle_in_radians=np.pi), Operator * pegg_barnett))
+   simulation = Simulation(
+      number_of_fock_states=30,
+      number_of_rotations=4,
+      number_of_parties=1,
+      initial_state_name='pegg-barnett',
+      number_of_parity_sectors=4)
+   qutip.plot_wigner(simulation.state_after_protocol)
+   plt.show()
+
